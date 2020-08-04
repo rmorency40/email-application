ebd9e15fb63ab3d39299d51d30bb764a924004af
rmorency40 <30129346+rmorency40@users.noreply.github.com>
2020-08-04 17:02:20 -0500

    Add code for email creation project

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,28 @@
 package emailapp;
 
 public class Email {
-
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+	
+    // Constructor to receive the  first name and last name
+	
+	public Email(String firstName, String lastName) {
+		this.firstName =  firstName;
+		this.lastName = lastName;
+		System.out.println("Email CREATED: " + this.firstName + " " + this.lastName);
+		
+	}
+	
+	// Ask for the  department
+	
+	//Aet the mailbox capacity
+	
+	//Set the alternate email address
+	
+	//Change the password
+	
 }
