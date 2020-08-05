2c618fd5b95eb29831648a1d25ed3aee14086a3b
rmorency40 <30129346+rmorency40@users.noreply.github.com>
2020-08-05 15:45:00 -0500

    Update code by adding ,ore methods to class Email

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,12 +1,17 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
 	private String firstName;
 	private String lastName;
 	private String password;
 	private String department;
+	private String email;
 	private int mailboxCapacity;
+	private int defaultPasswordLength = 10;
 	private String alternateEmail;
+	private String companySuffix = "aeycompany.com";
 	
     // Constructor to receive the  first name and last name
 	
@@ -15,11 +20,48 @@ public class Email {
 		this.lastName = lastName;
 		System.out.println("Email CREATED: " + this.firstName + " " + this.lastName);
 		
+		// Call a method asking for the department - return department
+		this.department = setDepartment();
+		System.out.println("Department: " + this.department);
+		
+		// Call a method that returns arandom password
+		this.password = randomPassword(defaultPasswordLength);
+		System.out.println("Your password is: " +this.password);
+		
+		//Combine element to nenerate teh email address
+		
+		email = firstName.toLowerCase() + '.' + lastName.toLowerCase() + "@" + department + "." + companySuffix;
+		System.out.println("Your email is: " +email);
 	}
 	
 	// Ask for the  department
 	
-	//Aet the mailbox capacity
+	private String setDepartment() {
+		System.out.print("DEPARTMENT CODES :\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department :");
+		Scanner in = new Scanner(System.in);
+		int depChoice = in.nextInt();
+		if (depChoice == 1) { return "sales";}
+		else if (depChoice == 2) { return "dev"; }
+		else if (depChoice == 3) { return "acct"; }
+		else {return ""; }
+		
+	}
+	
+	// Generate a random password
+	
+	private String randomPassword(int length) {
+		String passwordSet = "ABCDEFGHIGKLMNOPQRSTUVWXYZ0123456789@#$%";
+		char[] password = new char[length];
+		for (int i = 0; i < length; i++) {
+			int rand = (int)(Math.random() * passwordSet.length());
+			password[i] = passwordSet.charAt(rand);
+			//System.out.println(rand);
+			//System.out.println(passwordSet.charAt(rand));
+		}
+		return new String(password);
+	}
+	
+	//Set the mailbox capacity
 	
 	//Set the alternate email address
 	
