57dfd13a7da5d977abe30baf1de7b9c0db4f7edf
rmorency40 <30129346+rmorency40@users.noreply.github.com>
2020-08-10 18:49:59 -0500

    Adding more methods

--- src/studentsdatabaseApp/Student.java
+++ src/studentsdatabaseApp/Student.java
@@ -7,10 +7,11 @@ public class Student {
 	private String firstName;
 	private String lastName;
 	private int gradeYear;
-	private int studentID;
-	private String courses;
-	private String tuitionBalance;
-	private int costOfCourse = 600;
+	private String studentID;
+	private String courses = null;
+	private int  tuitionBalance = 0;
+	private static int costOfCourse = 600;
+	private static int id = 1000;
 	
 	
 	// Constructor prompts user to enter student's nad and year
@@ -24,13 +25,41 @@ public class Student {
 		
 		System.out.print("1 - Freshman\n2 - Softmore\n3 - Junior\n4 - Senior\nEnter student class level: ");
 		this.gradeYear = in.nextInt();
-		System.out.println(firstName + " " + lastName + " " + gradeYear);
+		
+		setStudentID();
+		
+		System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
 			
 	}
 	
 	//Generate the ID
+	private void setStudentID() {
+		
+		// grade level plus static ID
+		id++;
+		this.studentID = gradeYear + "" + id;
+	}
 	
 	//Enroll in courses
+	
+	public void enroll() {
+		do {
+				System.out.print("Enter course to enroll (Q to quit): ");
+				Scanner in =  new Scanner(System.in);
+				String course = in.nextLine();
+				if (!course.equals("Q")) {
+					courses = courses + "\n" + course;
+					tuitionBalance = tuitionBalance + costOfCourse;
+				}
+				else {
+					System.out.println("BREAK! ");
+					break;
+				}
+		} while (1 !=0);	
+		System.out.println("ENROLLED IN: " + courses);
+		System.out.println("TUITION BALANCE: " + tuitionBalance);
+	     
+	}		
 	
 	//View balance
 	
